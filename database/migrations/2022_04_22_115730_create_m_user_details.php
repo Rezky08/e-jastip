26b5639d2afff20dc4ae734bfff34fc058d04ba9
rezky08 <rezky.setiawan85@gmail.com>
2022-04-25 13:28:15 +0700

    [FEATURE]: - Preparation select payment method

--- database/migrations/2022_04_22_115730_create_m_user_details.php
+++ database/migrations/2022_04_22_115730_create_m_user_details.php
@@ -13,13 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('m_users', function (Blueprint $table) {
+        Schema::create('m_user_details', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedBigInteger('faculty_id');
+            $table->unsignedBigInteger('study_program_id');
+            $table->unsignedBigInteger('phone');
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('m_user_details');
     }
 };
