3421ab53f9651ca76e58d19832cee146b30bd2c5
rezky08 <rezky.setiawan85@gmail.com>
2022-06-08 00:55:20 +0700

    [BUGFIX]: - Place image to log

--- resources/views/pages/riwayat/log/index.blade.php
+++ resources/views/pages/riwayat/log/index.blade.php
@@ -16,17 +16,25 @@
                     @forelse($logItems as $index => $item)
                         <div class="">
                             <div class="row">
-                                <div class="col-1 d-flex justify-content-center position-relative">
-                                    <span class="position-absolute" style="bottom: 0">
-                                        <i class="fa fa-circle" aria-hidden="true"></i>
-                                    </span>
+                                <div class="col-1 d-flex justify-content-center align-items-center">
 
-                                    <div class="border-left h-50"></div>
+                                    <div class="border-left h-100 position-relative">
+                                        <span class="position-absolute" style="bottom: 0;transform: translate(-50%,50%)">
+                                            <i class="fa fa-circle" aria-hidden="true"></i>
+                                        </span>
+                                    </div>
                                 </div>
                                 <div class="col-11">
                                     <p>
                                         {{$item->remark}}
                                     </p>
+                                    <div class="d-flex" style="gap: 1rem">
+                                        @forelse($item->attachments as $attachment)
+                                            <x-wrapper.image name="log-image-{{$attachment->id}}"
+                                                             src="{{route('attachment',['attachment'=>$attachment])}}"/>
+                                        @empty
+                                        @endforelse
+                                    </div>
                                     <hr/>
 
                                 </div>
